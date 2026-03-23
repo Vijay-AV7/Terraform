5768b0903d1bbac9d0b73463453bff29d271ca39
Vijay <gkpk222@gmail.com>
2026-03-23 13:08:36 +0530

    simple EC2 instance create + Variable

---
+++ Count/Variable.tf
@@ -0,0 +1,30 @@
+/* 
+1. command line --> -var "<var-name>=<var-value>"
+2. tfvars
+3. env var
+4. default values
+5. user prompt */
+
+variable "ami_id" {
+ type = string
+  default = "ami-0220d79f3f480ecf5"
+}
+
+variable "environment" {
+    default = "dev" #default = "prod"
+}
+
+variable "instance_type" {
+ type = string
+  default = "t3.micro"
+}
+
+variable "ec2_tags" {
+    type = map
+    default = {
+        Project = "terraform" # Can be seen in Instance tag
+        Component = "backend" # Can be seen in Instance tag
+        Environment = "dev" # Can be seen in Instance tag
+        Name = "terraform-backend-dev" # Name of the instance and  Can be seen in Instance tag
+    }
+}
---
+++ Count/ec2.tf
@@ -0,0 +1,43 @@
+# Creating Security Group
+#Since we have provided region in provider file terraform will pick that
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls" #Security group name
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1" #-1 means all if -1 is used then from and to ports should be 0
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tag" # Name (refer security group for better understanding)
+  }
+}
+
+# to see the values in above resource output give type.name (eg:- aws_security_group.allow_tls) in output and you can take id or name any parameter
+
+    # output "security_group_info" {
+    #   value = aws_security_group.allow_tls.id 
+    # }
+
+# Creating Ec2 Instance
+resource "aws_instance" "instance" { # here aws_instance is resource type (predefined) cannot change. "instance" is resource name and its used to refer the resoruce 
+  count = 3
+  ami           = var.ami_id # This is devops-practice AMI ID
+  instance_type = var.environment == "dev" ? "t3.micro" : "t2.micro" #If yes t3.micro else t2.micro
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  tags = var.ec2_tags
+}
+
+output "security_group_info" {
+      value = aws_instance.instance 
+    }
