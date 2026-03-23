6631c0fe500cb4d21a5925d59663f139f2580d6b
Vijay <gkpk222@gmail.com>
2026-03-23 19:36:29 +0530

    simple EC2 instance create + Variable

--- DataSource/data.tf
+++ DataSource/data.tf
@@ -1,3 +1,4 @@
+# We use the data to get the values in runtime
 data "aws_ami" "RHEL" {
     most_recent      = true
     owners           = ["973714476881"]
@@ -22,9 +23,9 @@ data "aws_vpc" "default" {
   default = true
 }
 
-# output  "ami_id" {
-#   value       = data.aws_ami.RHEL.id
-# }
+output  "ami_id" {
+  value       = data.aws_ami.RHEL.id
+}
 
 output "default_vpc_id" {
     value = data.aws_vpc.default.id
