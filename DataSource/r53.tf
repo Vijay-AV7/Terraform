9796fe0b4a431bae5e7e57870f305f0123fde6da
Vijay <gkpk222@gmail.com>
2026-03-23 20:10:31 +0530

    datasource

--- DataSource/r53.tf
+++ DataSource/r53.tf
@@ -1,15 +1,20 @@
-resource "aws_route53_record" "heineken" {
-  zone_id = var.zone_id
-  name    = "${var.instance_type[count.index]}.${var.domain_name}" #Interpolation
-  type    = "A"
-  ttl     = 1
-  records        = [var.instance_type[count.index].private_ip] #We have to include private/public IP here
-}
+# aws_instance.instance ->  gives the instance info and we are refering public an private IP 
 
 resource "aws_route53_record" "heineken" {
+  count = length(var.instance_name)
   zone_id = var.zone_id
-  name    = var.domain_name #Interpolation
+  name    = "${var.instance_name[count.index]}.${var.domain_name}" #Interpolation
   type    = "A"
   ttl     = 1
-  records        = [var.instance_type[2].public_ip] #We have to include private/public IP here
+  records = [aws_instance.instance[count.index].private_ip] #We have to include private/public IP here
+  allow_overwrite = true
+}
+
+resource "aws_route53_record" "heineken_public" {
+  zone_id = var.zone_id
+  name    = var.domain_name
+  type    = "A"
+  ttl     = 1
+  records  = [aws_instance.instance[2].public_ip] #We have to include private/public IP here
+  allow_overwrite = true
 }
